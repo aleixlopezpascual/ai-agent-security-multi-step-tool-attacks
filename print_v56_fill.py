41712cb30e3b50e8fe4af88cb422761780d6c950
Aleix Lopez <aleix.lopez@glovoapp.com>
2026-09-02 10:28:32 +0200

    chore: clean up root directory by moving old reports and removing temporary/scratch files

--- print_v56_fill.py
+++
@@ -1,5 +0,0 @@
-import time
-from versions.v56_confused_deputy_interleaved import AttackAlgorithm
-
-algo = AttackAlgorithm()
-print("v56 algorithm loaded")
--- v48_mixed_hedge.py
+++
@@ -1,6 +0,0 @@
-from versions.v43_otv_adaptive import *
-
-class AttackAlgorithm(AttackAlgorithmBase):
-    def __init__(self, **kwargs):
-        self.config = kwargs
-        
